852cfd4703e75b80eab3e26eae461a0a9671a869
skai <tyronekisienya01@gmail.com>
2025-07-16 11:14:56 +0300

    native vue router transitions. Both fade and slide. Gotta pick between the two

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-lightpink min-h-screen text-gray-800 font-sans">
     <Navbar />
-    <transition name="slide" mode="out-in">
+    <transition name="fade" mode="out-in">
         <router-view />
     </transition>
     <Footer />
